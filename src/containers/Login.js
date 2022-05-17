d9cc2d4bb9d4510fad9dfc73142f5f61fa933ae2
Lester016 <lesterdeguzman1999@gmail.com>
2022-05-17 14:22:13 +0800

    Add Formik for registration and login pages

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = ({ error, loading }) => {
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email").required().label("Email"),
+    password: Yup.string().required().label("Password"),
+  });
+
+  return (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validationSchema={LoginSchema}
+      onSubmit={(values) => {
+        console.log(values.email, values.password);
+      }}
+    >
+      <Form>
+        <h1>Login</h1>
+        <Field type="email" name="email" />
+        <ErrorMessage name="email" component="div" />
+
+        <Field type={"password"} name="password" />
+        <ErrorMessage name="password" component="div" />
+
+        <p style={{ marginTop: 15, color: "red" }}>{error ? error : null}</p>
+
+        {loading ? <h1>loading...</h1> : <button>Sign in</button>}
+      </Form>
+    </Formik>
+  );
 };
 
 export default Login;
--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,7 +1,42 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-const Register = () => {
-  return <div>Register</div>;
+const Register = ({ loading, error }) => {
+  const RegisterSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email").required().label("Email"),
+    password: Yup.string().required().label("Password"),
+    confirmPassword: Yup.string()
+      .required()
+      .label("Confirm password")
+      .oneOf([Yup.ref("password"), null], "Password doesn't match"),
+  });
+
+  return (
+    <Formik
+      initialValues={{ email: "", password: "", confirmPassword: "" }}
+      validationSchema={RegisterSchema}
+      onSubmit={(values) => {
+        console.log(values.email, values.password);
+      }}
+    >
+      <Form>
+        <h1>Register</h1>
+        <Field type="email" name="email" />
+        <ErrorMessage name="email" component="div" />
+
+        <Field type={"password"} name="password" />
+        <ErrorMessage name="password" component="div" />
+
+        <Field type={"password"} name="confirmPassword" />
+        <ErrorMessage name="confirmPassword" component="div" />
+
+        <p style={{ marginTop: 15, color: "red" }}>{error ? error : null}</p>
+
+        {loading ? <h1>loading...</h1> : <button>Sign in</button>}
+      </Form>
+    </Formik>
+  );
 };
 
 export default Register;
