830f16919444f41c9e8b2eaa12fc22f689abd80c
Lester016 <lesterdeguzman1999@gmail.com>
2022-05-17 13:46:15 +0800

    Add react router v6

---
+++ src/containers/Fallback.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Fallback = () => {
+  return (
+    <main style={{ padding: "1rem" }}>
+      <p>(404) There's nothing here!</p>
+    </main>
+  );
+};
+
+export default Fallback;
