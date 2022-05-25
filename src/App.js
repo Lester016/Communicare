26117ca4fccb3d9af1edb0741fd1a6fa5e219a3e
Lester016 <lesterdeguzman1999@gmail.com>
2022-05-25 19:03:49 +0800

    Refactor downSampleBuffer

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Register from "./containers/Register";
 import ProtectedLayout from "./hoc/ProtectedLayout";
 import Contacts from "./containers/Contacts";
 import Logout from "./containers/Logout";
+import { downSampleBuffer } from "./utils/downSampleBuffer";
 
 // Hosted
 // https://communicare-server.herokuapp.com/
@@ -102,7 +103,7 @@ function App({ onAutoSignup, userID, email }) {
         processor.onaudioprocess = function (e) {
           let left = e.inputBuffer.getChannelData(0);
           // let left16 = convertFloat32ToInt16(left); // old 32 to 16 function
-          let left16 = downsampleBuffer(left, 44100, 16000);
+          let left16 = downSampleBuffer(left, 44100, 16000);
           socket.emit("binaryData", left16);
         };
         setStream(stream);
@@ -115,41 +116,6 @@ function App({ onAutoSignup, userID, email }) {
       setLocalTranscriptionMessage(null);
       socket.emit("endGoogleCloudStream");
     }
-  };
-
-  const downsampleBuffer = (buffer, sampleRate, outSampleRate) => {
-    if (outSampleRate === sampleRate) {
-      return buffer;
-    }
-    if (outSampleRate > sampleRate) {
-      // eslint-disable-next-line no-throw-literal
-      throw "down sampling rate show be smaller than original sample rate";
-    }
-
-    let sampleRateRatio = sampleRate / outSampleRate;
-    let newLength = Math.round(buffer.length / sampleRateRatio);
-    let result = new Int16Array(newLength);
-    let offsetResult = 0;
-    let offsetBuffer = 0;
-
-    while (offsetResult < result.length) {
-      let nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio);
-      let accum = 0,
-        count = 0;
-      for (
-        let i = offsetBuffer;
-        i < nextOffsetBuffer && i < buffer.length;
-        i++
-      ) {
-        accum += buffer[i];
-        count++;
-      }
-
-      result[offsetResult] = Math.min(1, accum / count) * 0x7fff;
-      offsetResult++;
-      offsetBuffer = nextOffsetBuffer;
-    }
-    return result.buffer;
   };
 
   const handleSubmitMessage = (message) => {
---
+++ src/utils/downSampleBuffer.js
@@ -0,0 +1,30 @@
+export const downSampleBuffer = (buffer, sampleRate, outSampleRate) => {
+  if (outSampleRate === sampleRate) {
+    return buffer;
+  }
+  if (outSampleRate > sampleRate) {
+    // eslint-disable-next-line no-throw-literal
+    throw "down sampling rate show be smaller than original sample rate";
+  }
+
+  let sampleRateRatio = sampleRate / outSampleRate;
+  let newLength = Math.round(buffer.length / sampleRateRatio);
+  let result = new Int16Array(newLength);
+  let offsetResult = 0;
+  let offsetBuffer = 0;
+
+  while (offsetResult < result.length) {
+    let nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio);
+    let accum = 0,
+      count = 0;
+    for (let i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
+      accum += buffer[i];
+      count++;
+    }
+
+    result[offsetResult] = Math.min(1, accum / count) * 0x7fff;
+    offsetResult++;
+    offsetBuffer = nextOffsetBuffer;
+  }
+  return result.buffer;
+};
