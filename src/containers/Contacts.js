a8a94c6c8565440d81d63b64d339faf4aa753f76
Lester016 <lesterdeguzman1999@gmail.com>
2022-05-26 09:14:02 +0800

    Implement Add contacts feautre

--- src/containers/Contacts.js
+++ src/containers/Contacts.js
@@ -1,9 +1,60 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Contacts = ({ onlineUsers, contactUser, userID }) => {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://communicare-4a0ec-default-rtdb.asia-southeast1.firebasedatabase.app/contacts/${userID}.json`
+      )
+      .then((response) => {
+        setContacts(response.data !== null ? response.data : []);
+      })
+      .catch((error) => console.log("error catched"));
+  }, []);
+
+  const addContactHandler = (contactID, contactEmail) => {
+    let updatedContacts = [
+      ...contacts,
+      { userID: contactID, email: contactEmail },
+    ];
+    axios
+      .put(
+        `https://communicare-4a0ec-default-rtdb.asia-southeast1.firebasedatabase.app/contacts/${userID}.json`,
+        updatedContacts
+      )
+      .then((response) => setContacts(updatedContacts))
+      .catch((error) => console.log("error catched: ", error));
+  };
+
+  const isInContactsHandler = (array, item) => {
+    for (let index = 0; index < array.length; index++) {
+      const element = array[index].userID;
+      // if (element === item) {
+      //   return true;
+      // }
+
+      if (element === item) {
+        return true;
+      }
+    }
+  };
+
   return (
     <div>
-      Contact
+      <h3>Your Contacts</h3>
+      <div style={{ border: "1px solid grey", marginBottom: 10 }}>
+        {contacts.map((user) => (
+          <div key={user.userID}>
+            <p style={{ color: "blue" }}>{user.email}</p>
+            <button onClick={() => contactUser(user.userID)}>Call</button>
+
+            <button onClick={() => {}}>Remove in contacts</button>
+          </div>
+        ))}
+      </div>
       <h3>Online Users</h3>
       {onlineUsers.map((user) => (
         <div
@@ -13,7 +64,15 @@ const Contacts = ({ onlineUsers, contactUser, userID }) => {
           {user.userID !== userID ? (
             <div>
               <p style={{ color: "brown" }}>{user.email}</p>
-              <button onClick={() => contactUser(user.userID)}>call</button>
+              <button onClick={() => contactUser(user.userID)}>Call</button>
+
+              {!isInContactsHandler(contacts, user.userID) && (
+                <button
+                  onClick={() => addContactHandler(user.userID, user.email)}
+                >
+                  Add into contacts
+                </button>
+              )}
             </div>
           ) : (
             <p style={{ color: "green" }}>{user.email} (You)</p>
