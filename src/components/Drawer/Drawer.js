3d5e08f5bb8bd7d061057ca34d518a4fdf6fe8e7
Jan Erickson Bataclan <jebataclan03@gmail.com>
2022-05-30 01:24:29 +0800

    Install necessary packages for UI, copy assets and components from forked

---
+++ src/components/Drawer/Drawer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Drawer as MUIDrawer } from '@mui/material/';
+
+const Drawer = ({ drawerItems }) => {
+  return (
+    <Drawer>Drawer</Drawer>
+  )
+}
+
+export default Drawer
