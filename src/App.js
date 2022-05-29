9d33993eedf153ddac36e116fe8b10921a429fb4
Lester016 <lesterdeguzman1999@gmail.com>
2022-05-29 17:46:08 +0800

    Add TURN server from Twilio

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import { downSampleBuffer } from "./utils/downSampleBuffer";
 // Hosted
 // https://communicare-server.herokuapp.com/
 // http://localhost:8000/
-const socket = io("http://localhost:8000/", {
+const socket = io("https://communicare-server.herokuapp.com/", {
   autoConnect: false,
 });
 
@@ -45,8 +45,31 @@ function App({ onAutoSignup, userID, email }) {
   const userMedia = useRef();
   const connectionRef = useRef();
   const iceConfig = [
-    { urls: "stun:stun.l.google.com:19302" },
-    { urls: "stun:global.stun.twilio.com:3478?transport=udp" },
+    {
+      url: "stun:global.stun.twilio.com:3478?transport=udp",
+      urls: "stun:global.stun.twilio.com:3478?transport=udp",
+    },
+    {
+      url: "turn:global.turn.twilio.com:3478?transport=udp",
+      username:
+        "d97bcd97906e5dbccb68f2fa4d3603732ec0fc00de27e5add6cec240c1aa433e",
+      urls: "turn:global.turn.twilio.com:3478?transport=udp",
+      credential: "owYhQ1O40vZmVE2SORWpy7v1j/j+n7kZ0WTlOWobwjc=",
+    },
+    {
+      url: "turn:global.turn.twilio.com:3478?transport=tcp",
+      username:
+        "d97bcd97906e5dbccb68f2fa4d3603732ec0fc00de27e5add6cec240c1aa433e",
+      urls: "turn:global.turn.twilio.com:3478?transport=tcp",
+      credential: "owYhQ1O40vZmVE2SORWpy7v1j/j+n7kZ0WTlOWobwjc=",
+    },
+    {
+      url: "turn:global.turn.twilio.com:443?transport=tcp",
+      username:
+        "d97bcd97906e5dbccb68f2fa4d3603732ec0fc00de27e5add6cec240c1aa433e",
+      urls: "turn:global.turn.twilio.com:443?transport=tcp",
+      credential: "owYhQ1O40vZmVE2SORWpy7v1j/j+n7kZ0WTlOWobwjc=",
+    },
   ];
 
   useEffect(() => {
