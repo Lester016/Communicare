9eccd47018ad079c952ed16317fc312993942d81
Lester016 <lesterdeguzman1999@gmail.com>
2022-05-30 08:26:13 +0800

    Create constant for ice servers

---
+++ src/constants/iceConfig.js
@@ -0,0 +1,27 @@
+export const iceConfig = [
+  {
+    url: "stun:global.stun.twilio.com:3478?transport=udp",
+    urls: "stun:global.stun.twilio.com:3478?transport=udp",
+  },
+  {
+    url: "turn:global.turn.twilio.com:3478?transport=udp",
+    username:
+      "d97bcd97906e5dbccb68f2fa4d3603732ec0fc00de27e5add6cec240c1aa433e",
+    urls: "turn:global.turn.twilio.com:3478?transport=udp",
+    credential: "owYhQ1O40vZmVE2SORWpy7v1j/j+n7kZ0WTlOWobwjc=",
+  },
+  {
+    url: "turn:global.turn.twilio.com:3478?transport=tcp",
+    username:
+      "d97bcd97906e5dbccb68f2fa4d3603732ec0fc00de27e5add6cec240c1aa433e",
+    urls: "turn:global.turn.twilio.com:3478?transport=tcp",
+    credential: "owYhQ1O40vZmVE2SORWpy7v1j/j+n7kZ0WTlOWobwjc=",
+  },
+  {
+    url: "turn:global.turn.twilio.com:443?transport=tcp",
+    username:
+      "d97bcd97906e5dbccb68f2fa4d3603732ec0fc00de27e5add6cec240c1aa433e",
+    urls: "turn:global.turn.twilio.com:443?transport=tcp",
+    credential: "owYhQ1O40vZmVE2SORWpy7v1j/j+n7kZ0WTlOWobwjc=",
+  },
+];
