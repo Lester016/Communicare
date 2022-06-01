f9b8cab7f6f726c5c0a387f770656a929c2b6b7e
Jan Erickson Bataclan <jebataclan03@gmail.com>
2022-06-01 19:12:56 +0800

    Initial UI by Jan for Transcribe

--- src/containers/Transcribe.js
+++ src/containers/Transcribe.js
@@ -3,6 +3,15 @@ import { connect } from "react-redux";
 import { downSampleBuffer } from "../utils/downSampleBuffer";
 import { getUserMedia } from "../utils/getUserMedia";
 
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+
+import List from "@mui/material/Box";
+import ListItem from "@mui/material/Box";
+
+import Typography from "../components/Typography";
+import Button from "../components/Button";
+
 let bufferSize = 2048;
 let AudioContext = window.AudioContext || window.webkitAudioContext;
 let context = new AudioContext({
@@ -63,29 +72,34 @@ const Transcribe = ({ userID, socket }) => {
   };
 
   return (
-    <div>
-      <h1>TRANSCRIBE</h1>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        width: { sm: `calc(100vw - 300px)` },
+        height: "100vh",
+        p: 4,
+        backgroundColor: "#F9FAFF",
+      }}>
+      <Box component={Paper} sx={{ height: "100%", display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "center", p: 2 }}>
+        <Typography sx={{ fontSize: "24px", fontWeight: "600" }}>Transcribe</Typography>
 
-      <div>
-        <ul>
-          <li>
-            Speak and this tool will transcribe the words spoken into written
-            text.
-          </li>
-          <li>
-            Make sure the speaking voice is clear for a better translation
-            quality.
-          </li>
-          <li>Rotate your phone for better usage.</li>
-          <li>Click the button to start transcribing.</li>
-        </ul>
-      </div>
+        <List component="ul" sx={{ listStylePosition: "inside", textAlign: "left", color: "#22BB72", fontSize: "16px", fontWeight: "600" }}>
+          <ListItem sx={{ display: "list-item" }}>Speak and this tool will transcribe the words spoken into written text.</ListItem>
+          <ListItem sx={{ display: "list-item" }}>Make sure the speaking voice is clear for a better translation quality.</ListItem>
+          <ListItem sx={{ display: "list-item" }}>Rotate your phone for better usage.</ListItem>
+          <ListItem sx={{ display: "list-item" }}>Click the button to start transcribing.</ListItem>
+        </List>
 
-      <button onClick={startLocalTranscription}>
-        {isTranscribeEnabled ? "Stop Transcribing" : "Transcribe now"}
-      </button>
-      <h3>{message && message}</h3>
-    </div>
+        <Button onClick={startLocalTranscription} sx={{ backgroundColor: isTranscribeEnabled ? "#6667AB" : "#BB223E" }}>Transcribe: {isTranscribeEnabled ? "On" : "Off"}</Button>
+
+        <Box sx={{ flex: 1, width: "100%", p: 4 }}>
+          <Box sx={{ width: "100%", height: "100%", backgroundColor: "#EAEFFF", borderRadius: 4, p: 4 }}>
+            <Typography sx={{fontSize: "24px", fontWeight: "500"}}>{console.log(message)}{message}</Typography>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
@@ -96,3 +110,29 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps)(Transcribe);
+
+/*
+<div>
+  <h1>TRANSCRIBE</h1>
+
+  <div>
+    <ul>
+      <li>
+        Speak and this tool will transcribe the words spoken into written
+        text.
+      </li>
+      <li>
+        Make sure the speaking voice is clear for a better translation
+        quality.
+      </li>
+      <li>Rotate your phone for better usage.</li>
+      <li>Click the button to start transcribing.</li>
+    </ul>
+  </div>
+
+  <button onClick={startLocalTranscription}>
+    {isTranscribeEnabled ? "Stop Transcribing" : "Transcribe now"}
+  </button>
+  <h3>{message && message}</h3>
+</div>
+*/
