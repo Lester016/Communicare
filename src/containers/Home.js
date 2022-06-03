fb66949cac7fea3be650f00e8cedf5a81bcc166a
Jan Erickson Bataclan <jebataclan03@gmail.com>
2022-06-03 19:47:24 +0800

    Revert condition for Call UI

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -130,7 +130,7 @@ const Home = ({
 
   return (
     <>
-      {true ? ( // ========================================== UI DURING A CALL ========================================== isCallAccepted && !isCallEnded
+      {isCallAccepted && !isCallEnded ? ( // ========================================== UI DURING A CALL ==========================================
         <Box
           component="main"
           sx={{
