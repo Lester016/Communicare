ab5847aeff094cb3f0ea309b30766a5e1efe08ef
Lester016 <lesterdeguzman1999@gmail.com>
2022-06-03 15:44:28 +0800

    Add more turn servers

--- src/constants/iceConfig.js
+++ src/constants/iceConfig.js
@@ -10,18 +10,18 @@ export const iceConfig = [
     urls: "turn:global.turn.twilio.com:3478?transport=udp",
     credential: "i83tkc6j6grt9/5OptNiA7AJLu8oeUcCX5e44M9Tawk=",
   },
-  // {
-  //   url: "turn:global.turn.twilio.com:3478?transport=tcp",
-  //   username:
-  //     "f661c54934e021901c7120911eae24d49ac35e0364dca20d673188c7a8743d34",
-  //   urls: "turn:global.turn.twilio.com:3478?transport=tcp",
-  //   credential: "i83tkc6j6grt9/5OptNiA7AJLu8oeUcCX5e44M9Tawk=",
-  // },
-  // {
-  //   url: "turn:global.turn.twilio.com:443?transport=tcp",
-  //   username:
-  //     "f661c54934e021901c7120911eae24d49ac35e0364dca20d673188c7a8743d34",
-  //   urls: "turn:global.turn.twilio.com:443?transport=tcp",
-  //   credential: "i83tkc6j6grt9/5OptNiA7AJLu8oeUcCX5e44M9Tawk=",
-  // },
+  {
+    url: "turn:global.turn.twilio.com:3478?transport=tcp",
+    username:
+      "f661c54934e021901c7120911eae24d49ac35e0364dca20d673188c7a8743d34",
+    urls: "turn:global.turn.twilio.com:3478?transport=tcp",
+    credential: "i83tkc6j6grt9/5OptNiA7AJLu8oeUcCX5e44M9Tawk=",
+  },
+  {
+    url: "turn:global.turn.twilio.com:443?transport=tcp",
+    username:
+      "f661c54934e021901c7120911eae24d49ac35e0364dca20d673188c7a8743d34",
+    urls: "turn:global.turn.twilio.com:443?transport=tcp",
+    credential: "i83tkc6j6grt9/5OptNiA7AJLu8oeUcCX5e44M9Tawk=",
+  },
 ];
