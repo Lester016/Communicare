34079ba6752a27a13d4a89aebd7275b1f0bde8b5
Lester016 <lesterdeguzman1999@gmail.com>
2022-06-05 21:48:21 +0800

    Fix syntax error

--- src/constants/iceConfig.js
+++ src/constants/iceConfig.js
@@ -1,25 +1,27 @@
 export const iceConfig = [
   {
-    url: 'stun:global.stun.twilio.com:3478?transport=udp',
-    urls: 'stun:global.stun.twilio.com:3478?transport=udp'
+    url: "stun:global.stun.twilio.com:3478?transport=udp",
+    urls: "stun:global.stun.twilio.com:3478?transport=udp",
   },
   {
-    url: 'turn:global.turn.twilio.com:3478?transport=udp',
-    username: '3a1f5285bf2d296a71d0c1149c94d368ae20d40a04544434f70cfa95405546a1',
-    urls: 'turn:global.turn.twilio.com:3478?transport=udp',
-    credential: '7z9Itfz22CrS+ySweeSKnJethbAkansHYTRkZ3AYyow='
+    url: "turn:global.turn.twilio.com:3478?transport=udp",
+    username:
+      "3a1f5285bf2d296a71d0c1149c94d368ae20d40a04544434f70cfa95405546a1",
+    urls: "turn:global.turn.twilio.com:3478?transport=udp",
+    credential: "7z9Itfz22CrS+ySweeSKnJethbAkansHYTRkZ3AYyow=",
   },
   {
-    url: 'turn:global.turn.twilio.com:3478?transport=tcp',
-    username: '3a1f5285bf2d296a71d0c1149c94d368ae20d40a04544434f70cfa95405546a1',
-    urls: 'turn:global.turn.twilio.com:3478?transport=tcp',
-    credential: '7z9Itfz22CrS+ySweeSKnJethbAkansHYTRkZ3AYyow='
+    url: "turn:global.turn.twilio.com:3478?transport=tcp",
+    username:
+      "3a1f5285bf2d296a71d0c1149c94d368ae20d40a04544434f70cfa95405546a1",
+    urls: "turn:global.turn.twilio.com:3478?transport=tcp",
+    credential: "7z9Itfz22CrS+ySweeSKnJethbAkansHYTRkZ3AYyow=",
   },
   {
-    url: 'turn:global.turn.twilio.com:443?transport=tcp',
-    username: '3a1f5285bf2d296a71d0c1149c94d368ae20d40a04544434f70cfa95405546a1',
-    urls: 'turn:global.turn.twilio.com:443?transport=tcp',
-    credential: '7z9Itfz22CrS+ySweeSKnJethbAkansHYTRkZ3AYyow='
-  }
-],
-;
+    url: "turn:global.turn.twilio.com:443?transport=tcp",
+    username:
+      "3a1f5285bf2d296a71d0c1149c94d368ae20d40a04544434f70cfa95405546a1",
+    urls: "turn:global.turn.twilio.com:443?transport=tcp",
+    credential: "7z9Itfz22CrS+ySweeSKnJethbAkansHYTRkZ3AYyow=",
+  },
+];
