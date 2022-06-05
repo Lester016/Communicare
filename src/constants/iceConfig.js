b89405bd219386643c94b625951e2b022ba26c2e
Lester016 <lesterdeguzman1999@gmail.com>
2022-06-05 21:43:43 +0800

    Update server config

--- src/constants/iceConfig.js
+++ src/constants/iceConfig.js
@@ -6,22 +6,22 @@ export const iceConfig = [
   {
     url: "turn:global.turn.twilio.com:3478?transport=udp",
     username:
-      "d97bcd97906e5dbccb68f2fa4d3603732ec0fc00de27e5add6cec240c1aa433e",
+      "f661c54934e021901c7120911eae24d49ac35e0364dca20d673188c7a8743d34",
     urls: "turn:global.turn.twilio.com:3478?transport=udp",
-    credential: "owYhQ1O40vZmVE2SORWpy7v1j/j+n7kZ0WTlOWobwjc=",
+    credential: "i83tkc6j6grt9/5OptNiA7AJLu8oeUcCX5e44M9Tawk=",
   },
-  // {
-  //   url: "turn:global.turn.twilio.com:3478?transport=tcp",
-  //   username:
-  //     "d97bcd97906e5dbccb68f2fa4d3603732ec0fc00de27e5add6cec240c1aa433e",
-  //   urls: "turn:global.turn.twilio.com:3478?transport=tcp",
-  //   credential: "owYhQ1O40vZmVE2SORWpy7v1j/j+n7kZ0WTlOWobwjc=",
-  // },
-  // {
-  //   url: "turn:global.turn.twilio.com:443?transport=tcp",
-  //   username:
-  //     "d97bcd97906e5dbccb68f2fa4d3603732ec0fc00de27e5add6cec240c1aa433e",
-  //   urls: "turn:global.turn.twilio.com:443?transport=tcp",
-  //   credential: "owYhQ1O40vZmVE2SORWpy7v1j/j+n7kZ0WTlOWobwjc=",
-  // },
+  {
+    url: "turn:global.turn.twilio.com:3478?transport=tcp",
+    username:
+      "f661c54934e021901c7120911eae24d49ac35e0364dca20d673188c7a8743d34",
+    urls: "turn:global.turn.twilio.com:3478?transport=tcp",
+    credential: "i83tkc6j6grt9/5OptNiA7AJLu8oeUcCX5e44M9Tawk=",
+  },
+  {
+    url: "turn:global.turn.twilio.com:443?transport=tcp",
+    username:
+      "f661c54934e021901c7120911eae24d49ac35e0364dca20d673188c7a8743d34",
+    urls: "turn:global.turn.twilio.com:443?transport=tcp",
+    credential: "i83tkc6j6grt9/5OptNiA7AJLu8oeUcCX5e44M9Tawk=",
+  },
 ];
