8ef5aae7f82ce2a0cf52f4508a02d21d6884e003
Lester016 <lesterdeguzman1999@gmail.com>
2022-06-05 21:47:31 +0800

    Update servers

--- src/constants/iceConfig.js
+++ src/constants/iceConfig.js
@@ -1,27 +1,25 @@
 export const iceConfig = [
   {
-    url: "stun:global.stun.twilio.com:3478?transport=udp",
-    urls: "stun:global.stun.twilio.com:3478?transport=udp",
+    url: 'stun:global.stun.twilio.com:3478?transport=udp',
+    urls: 'stun:global.stun.twilio.com:3478?transport=udp'
   },
   {
-    url: "turn:global.turn.twilio.com:3478?transport=udp",
-    username:
-      "f661c54934e021901c7120911eae24d49ac35e0364dca20d673188c7a8743d34",
-    urls: "turn:global.turn.twilio.com:3478?transport=udp",
-    credential: "i83tkc6j6grt9/5OptNiA7AJLu8oeUcCX5e44M9Tawk=",
+    url: 'turn:global.turn.twilio.com:3478?transport=udp',
+    username: '3a1f5285bf2d296a71d0c1149c94d368ae20d40a04544434f70cfa95405546a1',
+    urls: 'turn:global.turn.twilio.com:3478?transport=udp',
+    credential: '7z9Itfz22CrS+ySweeSKnJethbAkansHYTRkZ3AYyow='
   },
   {
-    url: "turn:global.turn.twilio.com:3478?transport=tcp",
-    username:
-      "f661c54934e021901c7120911eae24d49ac35e0364dca20d673188c7a8743d34",
-    urls: "turn:global.turn.twilio.com:3478?transport=tcp",
-    credential: "i83tkc6j6grt9/5OptNiA7AJLu8oeUcCX5e44M9Tawk=",
+    url: 'turn:global.turn.twilio.com:3478?transport=tcp',
+    username: '3a1f5285bf2d296a71d0c1149c94d368ae20d40a04544434f70cfa95405546a1',
+    urls: 'turn:global.turn.twilio.com:3478?transport=tcp',
+    credential: '7z9Itfz22CrS+ySweeSKnJethbAkansHYTRkZ3AYyow='
   },
   {
-    url: "turn:global.turn.twilio.com:443?transport=tcp",
-    username:
-      "f661c54934e021901c7120911eae24d49ac35e0364dca20d673188c7a8743d34",
-    urls: "turn:global.turn.twilio.com:443?transport=tcp",
-    credential: "i83tkc6j6grt9/5OptNiA7AJLu8oeUcCX5e44M9Tawk=",
-  },
-];
+    url: 'turn:global.turn.twilio.com:443?transport=tcp',
+    username: '3a1f5285bf2d296a71d0c1149c94d368ae20d40a04544434f70cfa95405546a1',
+    urls: 'turn:global.turn.twilio.com:443?transport=tcp',
+    credential: '7z9Itfz22CrS+ySweeSKnJethbAkansHYTRkZ3AYyow='
+  }
+],
+;
